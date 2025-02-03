ea921bcd2182ab4086c6978a4501916d3d6b6f6e
Bas van Beek <bas@tobin.nl>
2025-02-03 21:00:48 +0100

    improve redis handler

--- redis/config.go
+++ redis/config.go
@@ -3,6 +3,9 @@ package redis
 import (
 	"fmt"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/redis/go-redis/v9"
 
@@ -16,7 +19,7 @@ const (
 	defaultAddress = "localhost:6379"
 	defaultDB      = 0
 
-	Address  = "redis-address"
+	Hosts    = "redis-hosts"
 	DB       = "redis-db"
 	UserName = "redis-username"
 	Password = "redis-password"
@@ -26,7 +29,7 @@ const (
 type Config struct {
 	Prefix string
 
-	Address  []string
+	Hosts    []string
 	DB       int
 	UserName string
 	Password string
@@ -46,21 +49,42 @@ func (c *Config) Name() string {
 	return c.prefix("redis-pool")
 }
 
+func (c *Config) Initialize() {
+	if c.Hosts == nil {
+		c.Hosts = []string{defaultAddress}
+	}
+}
+
 // FlagSet implements run.Config.
 func (c *Config) FlagSet() *run.FlagSet {
 	flags := run.NewFlagSet("Redis options")
 
-	flags.StringArrayVar(&c.Address, c.prefix(Address),
-		[]string{defaultAddress}, "Redis server addresses")
+	if hoststr := os.Getenv("REDIS_HOSTS"); hoststr != "" {
+		c.Hosts = strings.Split(hoststr, ",")
+	}
+	if dbstr := os.Getenv("REDIS_DB"); dbstr != "" {
+		if db, err := strconv.ParseInt(dbstr, 10, 64); err == nil {
+			c.DB = int(db)
+		}
+	}
+	if user := os.Getenv("REDIS_USERNAME"); user != "" {
+		c.UserName = user
+	}
+	if pass := os.Getenv("REDIS_PASSWORD"); pass != "" {
+		c.Password = pass
+	}
+
+	flags.StringArrayVar(&c.Hosts, c.prefix(Hosts),
+		c.Hosts, "Redis server hosts")
 
 	flags.IntVar(&c.DB, c.prefix(DB),
-		defaultDB, "Redis database number")
+		c.DB, "Redis database number")
 
 	flags.StringVar(&c.UserName, c.prefix(UserName),
-		"", "Redis username")
+		c.UserName, "Redis username")
 
 	flags.SensitiveStringVar(&c.Password, c.prefix(Password),
-		"", "Redis password")
+		c.Password, "Redis password")
 
 	return flags
 }
@@ -69,11 +93,11 @@ func (c *Config) FlagSet() *run.FlagSet {
 func (c *Config) Validate() error {
 	var mErr error
 
-	for _, addr := range c.Address {
+	for _, addr := range c.Hosts {
 		if _, _, err := net.SplitHostPort(addr); err != nil {
 			mErr = multierror.Append(mErr,
-				flag.NewValidationError(c.prefix(Address),
-					fmt.Errorf("invalid address: %w", err)))
+				flag.NewValidationError(c.prefix(Hosts),
+					fmt.Errorf("invalid host: %w", err)))
 		}
 	}
 
@@ -83,7 +107,7 @@ func (c *Config) Validate() error {
 // PreRun implements run.PreRunner.
 func (c *Config) PreRun() error {
 	c.rdb = redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs:    c.Address,
+		Addrs:    c.Hosts,
 		Username: c.UserName,
 		Password: c.Password,
 	})
@@ -95,6 +119,7 @@ func (c *Config) PreRun() error {
 func (c *Config) Pool() redis.UniversalClient { return c.rdb }
 
 var (
-	_ run.Config    = (*Config)(nil)
-	_ run.PreRunner = (*Config)(nil)
+	_ run.Initializer = (*Config)(nil)
+	_ run.Config      = (*Config)(nil)
+	_ run.PreRunner   = (*Config)(nil)
 )
